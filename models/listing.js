c2c64f5f57d0998d6cde1d8eb4e9f49a171c2408
Ajay Dangi <ajaydangi2008@gmail.com>
2026-07-28 18:16:17 +0530

    intial Commit

---
+++ models/listing.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review.js");
+const review = require("./review.js");
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    
+   
+    image: {
+        filename: String,
+        url: String,
+    },
+    price: {
+        type: Number,
+    },
+    location: {
+        type: String,
+    },
+    country: {
+        type: String,
+    },
+    reviews: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Review",
+        }
+    ]
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if(listing) {
+        await review.deleteMany({_id : {$in: listing.reviews}});
+    } 
+});
+
+const Listing = mongoose.model("listing", listingSchema);
+module.exports = Listing;
