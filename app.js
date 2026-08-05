4144dff7826642e70cbae590f2551ab1718421c0
Ajay Dangi <ajaydangi2008@gmail.com>
2026-08-05 23:51:28 +0530

    Restructuring Review

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("./schema.js");
-const Review = require("./models/review.js")
+
 
 const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js")
 
 mongoose.connect('mongodb://127.0.0.1:27017/wanderlust')
   .then(() => console.log('Connection Successful'))
@@ -24,55 +22,9 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 
-
-const validateReview = (req, res, next) => {
-    let { error } = reviewSchema.validate(req.body);
-    if(error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-};
-
 app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews)
 
-//POST Reviews Route
-app.post("/listings/:id/reviews", validateReview, wrapAsync ( async(req, res, next) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listings/${listing._id}`);
-}));
-
-// Delete Review Route
-app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
-    let {id, reviewId} = req.params;
-
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/listings/${id}`);
-}));
-
-
-
-// app.get("/testListing", async (req, res) => {
-//     let sampleListing = new Listing ({
-//         title: "My New Villa",
-//         description:"By the beach",
-//         price: 1200,
-//         location:"Charu, jharkhnad",
-//         country: "India",
-//     });
-//     await sampleListing.save();
-//     console.log("Sample was saved");
-//     res.send("working fine");
-// });
 
 app.all("/*splat", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found"));
